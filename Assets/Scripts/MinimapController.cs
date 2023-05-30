b4fd4ff91ba72d1fc6141be668957a0a71b1fb63
atomeV8 <94994373+atomeV8@users.noreply.github.com>
2023-05-30 11:31:30 +0200

    update map

--- Assets/Scripts/MinimapController.cs
+++ Assets/Scripts/MinimapController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,17 @@ using UnityEngine;
 public class MinimapController : MonoBehaviour
 {
     GameObject[] mapPoints = new GameObject[7];
+    Vector2 posFinal;
+    Vector2 chemin;
+    float totalDelta;
+    int step = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        posFinal = new Vector2();
+        totalDelta = new float();
+
         mapPoints[0] = GameObject.Find("MapPointInfo");
         mapPoints[1] = GameObject.Find("MapPointBT");
         mapPoints[2] = GameObject.Find("MapPointMeca");
@@ -17,20 +25,41 @@ public class MinimapController : MonoBehaviour
         mapPoints[5] = GameObject.Find("MapPointMultimedia");
         mapPoints[6] = GameObject.Find("MapPointDehors");
 
-        foreach (Object obj in mapPoints)
-        {
-            Debug.Log(obj.ToString());
-        }
+        setCharacterPosition(UnityEngine.Random.Range(0, 6));
+        chemin = new Vector2();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        totalDelta += Time.deltaTime * 1000;
+        //Debug.Log(totalDelta);
+        if(step < 5)
+        {
+            if (totalDelta >= 1000 / 24)
+            {
+                Vector3 newPos = new Vector3() {
+                    x = this.gameObject.transform.position.x + chemin.x / 5 / 24,
+                    y = this.gameObject.transform.position.y + chemin.y / 5 / 24,
+                    z = 0
+                };
+                this.gameObject.transform.position = newPos;
+                totalDelta = 0;
+                step++;
+            }
+        }
     }
 
-    public void setCharacterPosition()
+    public void setCharacterPosition(int spot)
     {
-        this.gameObject.transform.position.x = mapPoints[0].gameObject.transform.position.x;
+        Debug.Log(mapPoints[spot].gameObject);
+        posFinal.x = mapPoints[spot].gameObject.transform.position.x;
+        posFinal.y = mapPoints[spot].gameObject.transform.position.y;
+        chemin.x = posFinal.x - this.gameObject.transform.position.x;
+        chemin.y = posFinal.y - this.gameObject.transform.position.y;
+        /*Debug.Log(mapPoints[spot].gameObject.transform.position.x);
+        Debug.Log(mapPoints[spot].gameObject.transform.position.y);
+        Debug.Log(chemin.x);
+        Debug.Log(chemin.y);*/
     }
 }
