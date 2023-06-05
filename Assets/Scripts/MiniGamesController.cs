13cdb798ea6877750844a91ed9c72f3d990ad05c
atomeV8 <94994373+atomeV8@users.noreply.github.com>
2023-06-05 13:02:17 +0200

    fix map

--- Assets/Scripts/MiniGamesController.cs
+++ Assets/Scripts/MiniGamesController.cs
@@ -1,12 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using TIBLibrary;
 
 public class MiniGamesController : MonoBehaviour
 {
-    
+
     void Start()
     {
         if (StaticGameData.isLost)
--- Assets/Scripts/MinimapController.cs
+++ Assets/Scripts/MinimapController.cs
@@ -1,14 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MinimapController : MonoBehaviour
 {
-    GameObject[] mapPoints = new GameObject[7];
+    GameObject[] mapPoints = new GameObject[8];
     Vector2 posFinal;
     Vector2 chemin;
     int step = 0;
+    int stepsUntilFinished = 80;
+    float frameRate = 60f;
     Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -17,40 +16,55 @@ public class MinimapController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         posFinal = new Vector3();
 
-        mapPoints[0] = GameObject.Find("MapPointInfo");
-        mapPoints[1] = GameObject.Find("MapPointBT");
-        mapPoints[2] = GameObject.Find("MapPointMeca");
-        mapPoints[3] = GameObject.Find("MapPointElectro");
-        mapPoints[4] = GameObject.Find("MapPointAuto");
-        mapPoints[5] = GameObject.Find("MapPointMultimedia");
-        mapPoints[6] = GameObject.Find("MapPointDehors");
-
-        setCharacterPosition(UnityEngine.Random.Range(0, 6));
+        mapPoints[1] = GameObject.Find("MapPointInfo");
+        mapPoints[2] = GameObject.Find("MapPointBT");
+        mapPoints[3] = GameObject.Find("MapPointMeca");
+        mapPoints[4] = GameObject.Find("MapPointElectro");
+        mapPoints[5] = GameObject.Find("MapPointAuto");
+        mapPoints[6] = GameObject.Find("MapPointMultimedia");
+        mapPoints[7] = GameObject.Find("MapPointDehors");
         chemin = new Vector2();
+        Time.fixedDeltaTime = 1f / frameRate;
+
+        if (PlayerPrefs.GetInt("MapPosition") == 0)
+        {
+            PlayerPrefs.SetInt("MapPosition", 7);
+        }
+
+        gameObject.transform.position = mapPoints[PlayerPrefs.GetInt("MapPosition")].transform.position;
+
+        setCharacterPosition();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(step < 5)
+        if (step < stepsUntilFinished)
         {
-                rb.MovePosition(rb.position + chemin);
-                Debug.Log(rb.position);
-                step++;
+            //Debug.Log(chemin);
+            rb.MovePosition(rb.position + chemin);
+            step++;
         }
     }
 
-    public void setCharacterPosition(int spot)
+    public void setCharacterPosition()
     {
-        //Debug.Log(mapPoints[spot].gameObject);
-        //Debug.Log("Position du personnage avant : " + gameObject.transform.position);
-        posFinal.x = mapPoints[spot].gameObject.transform.position.x;
-        posFinal.y = mapPoints[spot].gameObject.transform.position.y;
-        chemin.x = posFinal.x - gameObject.transform.position.x;
-        chemin.y = posFinal.y - gameObject.transform.position.y;
-        /*Debug.Log(mapPoints[spot].gameObject.transform.position.x);
-        Debug.Log(mapPoints[spot].gameObject.transform.position.y);
-        Debug.Log(chemin.x);
-        Debug.Log(chemin.y);*/
+        int spot;
+        while (true)
+        {
+            spot = Random.Range(1, 8);
+
+            if (spot != PlayerPrefs.GetInt("MapPosition"))
+            {
+                posFinal.x = mapPoints[spot].gameObject.transform.position.x;
+                posFinal.y = mapPoints[spot].gameObject.transform.position.y;
+                chemin.x = (posFinal.x - gameObject.transform.position.x) / stepsUntilFinished;
+                chemin.y = (posFinal.y - gameObject.transform.position.y) / stepsUntilFinished;
+                PlayerPrefs.SetInt("MapPosition", spot);
+                return;
+            }
+        }
+
+
     }
 }
